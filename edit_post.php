08d90834243865a871baecaa8df9c9627a5a9b5a
Nathan <nathanpulkis@gmail.com>
2022-02-19 18:49:04 +0000

    Added ability to edit post

---
+++ edit_post.php
@@ -0,0 +1,69 @@
+<?php
+
+// Get the record data
+$post_id = filter_input(INPUT_POST, 'post_id', FILTER_VALIDATE_INT);
+$tag_id = filter_input(INPUT_POST, 'tag_id', FILTER_VALIDATE_INT);
+$caption = filter_input(INPUT_POST, 'caption');
+$likes = filter_input(INPUT_POST, 'likes', FILTER_VALIDATE_FLOAT);
+
+// Validate inputs
+if ($post_id == NULL || $post_id == FALSE || $post_id == NULL ||
+    $post_id == FALSE || empty($caption) ||
+    $likes == NULL || $likes == FALSE) {
+    $error = "Invalid record data. Check all fields and try again.";
+    include('error.php');
+} else {
+
+    /**************************** Image upload ****************************/
+
+    $imgFile = $_FILES['image']['name'];
+    $tmp_dir = $_FILES['image']['tmp_name'];
+    $imgSize = $_FILES['image']['size'];
+    $original_image = filter_input(INPUT_POST, 'original_image');
+
+    if ($imgFile) {
+        $upload_dir = 'image_uploads/'; // upload directory
+        $imgExt = strtolower(pathinfo($imgFile, PATHINFO_EXTENSION)); // get image extension
+        $valid_extensions = array('jpeg', 'jpg', 'png', 'gif'); // valid extensions
+        $image = rand(1000, 1000000) . "." . $imgExt;
+        if (in_array($imgExt, $valid_extensions)) {
+            if ($imgSize < 5000000) {
+                if (filter_input(INPUT_POST, 'original_image') !== "") {
+                    unlink($upload_dir . $original_image);
+                }
+                move_uploaded_file($tmp_dir, $upload_dir . $image);
+            } else {
+                $errMSG = "Sorry, your file is too large it should be less then 5MB";
+            }
+        } else {
+            $errMSG = "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+        }
+    } else {
+// if no image selected the old image remain as it is.
+        $image = $original_image; // old image from database
+    }
+
+    /************************** End Image upload **************************/
+
+// If valid, update the record in the database
+    require_once('database.php');
+
+    $query = 'UPDATE posts
+SET tagID = :tag_id,
+caption = :caption,
+likes = :likes,
+image = :image
+WHERE postID = :post_id';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':tag_id', $tag_id);
+    $statement->bindValue(':caption', $caption);
+    $statement->bindValue(':likes', $likes);
+    $statement->bindValue(':image', $image);
+    $statement->bindValue(':post_id', $post_id);
+    $statement->execute();
+    $statement->closeCursor();
+
+// Display the Product List page
+    include('index.php');
+}
+?>
---
+++ edit_post_form.php
@@ -0,0 +1,47 @@
+<?php
+require('database.php');
+
+$post_id = filter_input(INPUT_POST, 'post_id', FILTER_VALIDATE_INT);
+$query = 'SELECT *
+          FROM posts
+          WHERE postID = :post_id';
+$statement = $db->prepare($query);
+$statement->bindValue(':post_id', $post_id);
+$statement->execute();
+$posts = $statement->fetch(PDO::FETCH_ASSOC);
+$statement->closeCursor();
+?>
+    <!-- the head section -->
+
+<?php
+include('includes/header.php');
+?>
+<div class="container">
+
+    <form action="edit_post.php" method="post" enctype="multipart/form-data"
+    id="edit_record_form">
+        <input type="hidden" name="original_image" value="<?php echo $posts['image']; ?>" />
+        <input type="hidden" name="post_id"
+               value="<?php echo $posts['postID']; ?>">
+        <input type="hidden" name="likes"
+               value="<?php echo $posts['likes']; ?>">
+        <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Edit Tag Id</label>
+            <input type="text" class="form-control" id="tagID" name="tag_id" placeholder="Enter Tag ID" value="<?php echo $posts['tagID'];?>">
+        </div>
+        <div class="mb-3">
+            <label for="exampleFormControlTextarea1" class="form-label">Edit Caption</label>
+            <textarea class="form-control" id="caption" name="caption" rows="5" ><?php echo $posts['caption'];?></textarea>
+        </div>
+
+        <div class="mb-3">
+            <input type="file" name="image" class="form-control" id="image" aria-describedby="inputGroupFileAddon04" aria-label="Upload">
+            <?php if ($posts['image'] != "") { ?>
+                <p><img src="./image_uploads/<?php echo $posts['image']; ?>" height="150" /></p>
+            <?php } ?>
+        </div>
+        <button type="submit" class="btn btn-sm btn-primary" role="button">Save Changes</button>
+    </form>
+
+
+</div>
