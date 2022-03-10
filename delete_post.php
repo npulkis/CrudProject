52f524cb6e80c9d4f6cc99c7739e2b122f786a06
nthnplks21 <nathanpulkis@gmail.com>
2022-03-10 14:18:10 +0000

    Changed delete_post.php to accept id from url using _GET

--- delete_post.php
+++ delete_post.php
@@ -2,19 +2,14 @@
 require_once('database.php');
 
 // Get IDs
-$post_id = filter_input(INPUT_POST, 'post_id', FILTER_VALIDATE_INT);
-$tag_id = filter_input(INPUT_POST, 'tag_id', FILTER_VALIDATE_INT);
-
+$post_id =$_GET['id'];
 // Delete the product from the database
-if ($post_id != false && $tag_id != false) {
     $query = "DELETE FROM posts
-              WHERE postID = :post_id";
+              WHERE postID = '$post_id'";
     $statement = $db->prepare($query);
-    $statement->bindValue(':post_id', $post_id);
     $statement->execute();
     $statement->closeCursor();
-}
 
-// display the Product List page
-include('index.php');
+// redirect to  the Post List page
+echo("<script>window.location.replace('index.php');</script>");
 ?>
